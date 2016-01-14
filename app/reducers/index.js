58fe22b52cc335bf0cb1e53eb07a7fa399c28550
Stephen Saunders <saunders.stephen@gmail.com>
2016-01-14 11:22:53 -0800

    make root reducer a combined reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-module.exports = {
+var combineReducers = require('redux').combineReducers;
+
+var rootReducer = combineReducers({
   user: require('./user-reducer'),
   newRace: require('./new-race-reducer'),
-  activeRace: require('./active-race-reducer')
-};
+  activeRace: require('./active-race-reducer'),
+  uiReducer: require('./ui-reducer')
+});
+
+module.exports = rootReducer
