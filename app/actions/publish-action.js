7467c2fcbaa4540488fa042c7cfe2adc56e9c492
Stephen Saunders <saunders.stephen@gmail.com>
2016-01-14 22:30:07 -0800

    change library used on actions

--- app/actions/publish-action.js
+++ app/actions/publish-action.js
@@ -4,7 +4,7 @@
 var type = require('../constants').action.type;
 var http = require('rest');
 
-var store = require('../store/configureStore.js');
+var store = require('../store/configureStore');
 
 var CENTER_MAP = require('../constants').action.CENTER_MAP;
 var ADD_WAYPOINT = require('../constants').action.ADD_WAYPOINT;
