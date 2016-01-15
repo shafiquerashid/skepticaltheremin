2654d416a827ae6cda4beeaa6654dd4b660522a3
Stephen Saunders <saunders.stephen@gmail.com>
2016-01-14 22:32:46 -0800

    Consolidate reducer logic to states

---
+++ app/reducers/view-reducer.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var _ = require('lodash');
+
+var NAV_RACE = require('../constants').action.NAV_RACE;
+var NAV_SUMMARY = require('../constants').action.NAV_SUMMARY;
+var NAV_CREATE_RACE = require('../constants').action.NAV_CREATE_RACE;
+var NAV_CREATED_RACES = require('../constants').action.NAV_CREATED_RACES;
+var CREATE = require('../constants').view.CREATE;
+var RACE = require('../constants').view.RACE;
+var SUMMARY = require('../constants').view.SUMMARY;
+var CREATED_RACE_LIST = require('../constants').view.CREATED_RACE_LIST;
+
+
+function viewReducer (state, action) {
+  state = state || CREATE;
+	switch (action.type) {
+		case NAV_CREATE_RACE:
+			return _.extend({}, state, {
+				view: CREATE
+			});
+		case NAV_CREATED_RACES:
+			return _.extend({}, state, {
+				view: CREATED_RACE_LIST
+			});
+		case NAV_RACE:
+			return _.extend({}, state, {
+				view: RACE
+			});
+		case NAV_SUMMARY:
+			return _.extend({}, state, {
+				view: SUMMARY
+			});
+		default:
+			return state;
+	}
+}
+
+module.exports = viewReducer;
